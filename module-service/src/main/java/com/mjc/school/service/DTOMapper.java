2640ffccfafc032e21572c736f3e0832bc1c79a1
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-09-23 22:01:11 +0200

    improve dto

--- module-service/src/main/java/com/mjc/school/service/DTOMapper.java
+++ module-service/src/main/java/com/mjc/school/service/DTOMapper.java
@@ -7,13 +7,18 @@ import com.mjc.school.service.dto.implementation.NewsDTORespImpl;
 import com.mjc.school.repository.model.implementation.AuthorModel;
 import com.mjc.school.repository.model.implementation.NewsModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface DTOMapper {
     DTOMapper instance = Mappers.getMapper(DTOMapper.class);
     NewsDTORespImpl newsToDto(NewsModel newsEntity);
+    @Mapping(ignore = true, target = "createDate")
+    @Mapping(ignore = true, target = "lastUpdateDate")
     NewsModel newsReqToEntity(NewsDTOReqImpl newsDTOResp);
     AuthorDTORespImpl authorToAuthorDto(AuthorModel authorEntity);
+    @Mapping(ignore = true, target = "createDate")
+    @Mapping(ignore = true, target = "lastUpdateDate")
     AuthorModel authorReqToEntity(AuthorDTOReqImpl authorDTOResp);
 }
--- module-service/src/main/java/com/mjc/school/service/dto/implementation/AuthorDTORespImpl.java
+++ module-service/src/main/java/com/mjc/school/service/dto/implementation/AuthorDTORespImpl.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Setter
 @Getter
@@ -14,4 +15,11 @@ public class AuthorDTORespImpl extends AuthorDTOReqImpl implements AuthorDTOResp
     private LocalDateTime createDate;
     private LocalDateTime lastUpdateDate;
     private static final String dateFormatPattern = "yyyy-MM-dd'T'HH:mm:ss.SSS";
+
+    @Override
+    public String toString() {
+        return "[" + id + "] " + name + ". created: " +
+                createDate.format(DateTimeFormatter.ofPattern(dateFormatPattern)) + ". modified: " +
+                lastUpdateDate.format(DateTimeFormatter.ofPattern(dateFormatPattern));
+    }
 }
