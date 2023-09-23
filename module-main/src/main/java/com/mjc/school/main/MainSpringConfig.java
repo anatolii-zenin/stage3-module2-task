54ee1a0803b1dfb6d2f6536b5706a2f669a51352
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-09-23 22:01:44 +0200

    remove dependency on dto module

--- module-main/src/main/java/com/mjc/school/main/MainSpringConfig.java
+++ module-main/src/main/java/com/mjc/school/main/MainSpringConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"com.mjc.school.main", "com.mjc.school.controller", "com.mjc.school.dto"})
+@ComponentScan({"com.mjc.school.main", "com.mjc.school.controller"})
 public class MainSpringConfig {
 }
