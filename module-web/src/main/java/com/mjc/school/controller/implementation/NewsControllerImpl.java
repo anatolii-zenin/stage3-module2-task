43d92d465899b747b4b87edf1c94cbb61d84b03a
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-09-25 16:02:01 +0200

    [UGLY] duplicate code to satisfy tests

--- module-web/src/main/java/com/mjc/school/controller/implementation/NewsControllerImpl.java
+++ module-web/src/main/java/com/mjc/school/controller/implementation/NewsControllerImpl.java
@@ -4,12 +4,38 @@ import com.mjc.school.controller.NewsController;
 import com.mjc.school.service.dto.NewsDTOReq;
 import com.mjc.school.service.dto.NewsDTOResp;
 import com.mjc.school.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+
 @Controller
-public class NewsControllerImpl extends BaseControllerImpl<NewsDTOReq, NewsDTOResp, NewsService>
-        implements NewsController {
-    public NewsControllerImpl(NewsService newsService) {
-        this.service = newsService;
+public class NewsControllerImpl implements NewsController {
+    @Autowired
+    NewsService service;
+
+    @Override
+    public List<NewsDTOResp> readAll() {
+        return service.readAll();
+    }
+
+    @Override
+    public NewsDTOResp readById(Long id) {
+        return service.readById(id);
+    }
+
+    @Override
+    public NewsDTOResp create(NewsDTOReq createRequest) {
+        return service.create(createRequest);
+    }
+
+    @Override
+    public NewsDTOResp update(NewsDTOReq updateRequest) {
+        return service.update(updateRequest);
+    }
+
+    @Override
+    public boolean deleteById(Long id) {
+        return service.deleteById(id);
     }
 }
